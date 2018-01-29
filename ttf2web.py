12cb31b1a3fe8acf491de46738458ace6c25480d
johncf <johncf@protonmail.com>
2018-01-29 13:12:46 +0530

    refactor, correct css generation

--- ttf2web.py
+++ ttf2web.py
@@ -5,24 +5,41 @@ import sys
 from fontTools import ttLib
 from subprocess import Popen
 
-def main(fontfile, assetdir="assets", subsetfile=None, cssfile=None):
+def readSubsetFile(subsetfile):
+    subsets = []
+    with open(subsetfile, 'r') as subsethandle:
+        for line in subsethandle:
+            subset = line.split()
+            if len(subset) == 2:
+                subsets.append(subset)
+            elif len(subset) > 0:
+                raise Exception('A line with ' + len(subset) + ' fields found (expected 2) in the subset file')
+    return subsets
+
+def getDefaultSubsets():
+    subsetfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'subsets')
+    return readSubsetFile(subsetfile)
+
+def main(fontfile, assetdir="assets", subsetfile=None, cssfile=None, fontstyle=None, fontweight=None):
     font = ttLib.TTFont(fontfile)
-    familyname = font['name'].names[1].toStr()
-    stylename = font['name'].names[2].toStr()
+    fontfamily = font['name'].getDebugName(1)
+    subfamily = font['name'].getDebugName(2)
     font.close()
-    basename = os.path.splitext(os.path.basename(fontfile))[0]
-    os.makedirs(assetdir, exist_ok=True)
+    if fontstyle is None:
+        fontstyle = 'italic' if subfamily == 'Italic' else 'normal'
+    if fontweight is None:
+        fontweight = '700' if subfamily == 'Bold' else '400'
     if subsetfile is None:
-        subsetfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'subsets')
+        subsets = getDefaultSubsets()
+    else:
+        subsets = readSubsetFile(subsetfile)
+    basename = os.path.splitext(os.path.basename(fontfile))[0]
     if cssfile is None:
         cssfile = basename + '.css'
-    with open(cssfile, 'w') as csshandle, open(subsetfile) as subsethandle:
+    with open(cssfile, 'w') as csshandle:
+        os.makedirs(assetdir, exist_ok=True)
         print("Generating WebFonts...")
-        for subset in subsethandle:
-            subset = subset.strip()
-            if not subset:
-                continue
-            charset, unicodes = subset.split()
+        for charset, unicodes in subsets:
             outfile = os.path.join(assetdir, basename + "." + charset + ".woff2")
             print(outfile)
             p = Popen(['pyftsubset', fontfile,
@@ -32,9 +49,10 @@ def main(fontfile, assetdir="assets", subsetfile=None, cssfile=None):
             p.communicate()
             print('/* ' + charset + ' */', file=csshandle)
             print('@font-face {', file=csshandle)
-            print('  font-family: "' + familyname + '";', file=csshandle)
-            print('  font-style: "' + stylename + '";', file=csshandle)
-            print('  src: local("' + familyname + '"), url(' + outfile + ') format("woff2");', file=csshandle)
+            print('  font-family: "' + fontfamily + '";', file=csshandle)
+            print('  font-style: "' + fontstyle + '";', file=csshandle)
+            print('  font-weight: "' + fontweight + '";', file=csshandle)
+            print('  src: local("' + fontfamily + '"), url(' + outfile + ') format("woff2");', file=csshandle)
             print('  unicode-range: ' + unicodes + ';', file=csshandle)
             print('}\n', file=csshandle)
 
